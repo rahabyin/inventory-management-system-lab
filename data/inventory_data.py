865a48b54645653223a357e61dd2e8369f68658a
rahabyin <rahabnwanja@gmail.com>
2026-07-04 15:48:31 +0300

    created a mock inventory db

---
+++ data/inventory_data.py
@@ -0,0 +1,16 @@
+inventory = [
+    {
+        "id": 1,
+        "name": "Organic Almond Milk",
+        "brand": "Silk",
+        "price": 4.99,
+        "stock": 15
+    },
+    {
+        "id": 2,
+        "name": "Whole Wheat Bread",
+        "brand": "Nature's Own",
+        "price": 2.99,
+        "stock": 20
+    }
+]
