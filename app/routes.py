73a0a5a3b0295b05caddd036a8ed81344f3ecd32
rahabyin <rahabnwanja@gmail.com>
2026-07-04 16:42:54 +0300

    added GET inventory by ID endpoint

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,12 @@ def home():
 @inventory_bp.route("/inventory", methods=["GET"])
 def get_inventory():
     return jsonify(inventory), 200
+
+@inventory_bp.route("/inventory/<int:item_id>", methods=["GET"])
+def get_item(item_id):
+    item = next((i for i in inventory if i["id"] == item_id), None)
+
+    if item is None:
+        return jsonify({"error": "Item not found"}), 404
+
+    return jsonify(item), 200
